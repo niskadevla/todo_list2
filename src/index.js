7127e1bbb9f280e746ea4559e2ccf24f770422d2
Denis <niskadevla@gmail.com>
2020-07-12 15:55:41 +0300

    Added new package - Redux

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <div className="wrap">      
+    <div className="wrap">
       <App />
     </div>
   </React.StrictMode>,
